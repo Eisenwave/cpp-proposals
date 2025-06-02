5b851d81dd23af1aca42a722c1a24c0700b5ad90
Eisenwave <me@eisenwave.net>
2025-06-02 11:11:12 +0200

    improve design and reference implementation

--- src/intdiv.cpp
+++ src/intdiv.cpp
@@ -1,3 +1,5 @@
+#include <type_traits>
+
 template<class T>
 struct div_result {
     T quotient;
@@ -5,32 +7,39 @@ struct div_result {
     friend bool operator<=>(const div_result&, const div_result&) = default;
 };
 
-constexpr int __sgn2(int x) {
-    // Equivalent to: (x >> (INT_WIDTH - 1)) | 1
-    return x < 0 ? -1 : 1;
+template<class T>
+constexpr T __sgn2(T x) {
+    if constexpr (std::is_signed_v<T>) {
+        // Equivalent to: (x >> (INT_WIDTH - 1)) | 1
+        return x < 0 ? -1 : 1;    
+    } else {
+        return 1;
+    }
 }
 
-constexpr div_result<int> __div_rem_impl(
-    // Dividend.
-    int x,
-    // Divisor.
-    int y,
-    // If true, truncating division gives us the wrong result,
-    // and increasing the magnitude of the quotient by one would be correct.
-    bool increment,
-    // The quotient sign (1 or -1).
-    int quotient_sign
-) {
-    int quotient_change = int(increment) * quotient_change;
-    return {
-        .quotient = x / y + quotient_change,
-        // This is (x % y - quotient_change * y),
-        // except that we use unsigned int to avoid overflow when y is INT_MIN.
-        // Due to modular arithmetic rules, when we multiply y with -1,
-        // the remainder is congruent to (x % y + y),
-        // so simply doing it all with unsigned integers fixes our overflow problems.
-        .remainder = int(unsigned(x % y) - unsigned(quotient_change) * unsigned(y))
-    };
+/// Given a dividend x, divisor y, and quotient offset d (-1, 0, or 1),
+/// returns (x / y + d) as the quotient,
+/// and a remainder of a division between x and y
+/// that would have yielded that quotient.
+template<class T>
+constexpr div_result<T> __div_rem_offset_quotient(T x, T y, T d) {
+    if constexpr (std::is_signed_v<T>) {
+        using U = std::make_unsigned_t<T>;
+        return {
+            .quotient = x / y + d,
+            // This is (x % y - d * y),
+            // except that we use unsigned int to avoid overflow when y is INT_MIN.
+            // Due to modular arithmetic rules, when we multiply y with -1,
+            // the remainder is congruent to (x % y + y),
+            // so simply doing it all with unsigned integers fixes our overflow problems.
+            .remainder = T(U(x % y) - U(d) * U(y))
+        };
+    } else {
+        return { 
+            .quotient = x / y + d,
+            .remainder = x % y - d * y
+        };
+    }
 }
 
 // Idea: trivial implementation.
@@ -48,7 +57,7 @@ constexpr int div_to_zero(int x, int y) {
 constexpr div_result<int> div_rem_away_zero(int x, int y) {
     int quotient_sign = __sgn2(x) * __sgn2(y);
     bool increment = x % y != 0;
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_away_zero(int x, int y) {
@@ -59,7 +68,7 @@ constexpr int div_away_zero(int x, int y) {
 //       the result is one greater than what we want
 //       for negative quotients, unless the remainder is zero.
 constexpr div_result<int> div_rem_to_inf(int x, int y) {
-    int quotient_positive = (x ^ y) >= 0;
+    bool quotient_positive = (x ^ y) >= 0;
     bool adjust = x % y != 0 && quotient_positive;
     return {
         .quotient  = x / y + int(adjust),
@@ -93,7 +102,7 @@ constexpr int div_to_neg_inf(int x, int y) {
 constexpr div_result<int> div_rem_to_odd(int x, int y) {
     int quotient_sign = __sgn2(x) * __sgn2(y);
     bool increment = x % y != 0 && x / y % 2 == 0;
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_to_odd(int x, int y) {
@@ -106,7 +115,7 @@ constexpr int div_to_odd(int x, int y) {
 constexpr div_result<int> div_rem_to_even(int x, int y) {
     int quotient_sign = __sgn2(x) * __sgn2(y);
     bool increment = x % y != 0 && x / y % 2 != 0;
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_to_even(int x, int y) {
@@ -121,7 +130,7 @@ constexpr div_result<int> div_rem_ties_to_zero(int x, int y) {
     int abs_rem = x % y * __sgn2(x);
     int abs_half_y = y / 2 * __sgn2(y);
     bool increment = abs_rem > abs_half_y;
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_ties_to_zero(int x, int y) {
@@ -143,7 +152,7 @@ constexpr div_result<int> div_rem_ties_away_zero(int x, int y) {
     int abs_rem = x % y * __sgn2(x);
     int abs_half_y = y / 2 * __sgn2(y);
     bool increment = abs_rem >= abs_half_y + int(y % 2 != 0);
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_ties_away_zero(int x, int y) {
@@ -157,7 +166,7 @@ constexpr div_result<int> div_rem_ties_to_inf(int x, int y) {
     int abs_rem = x % y * __sgn2(x);
     int abs_half_y = y / 2 * __sgn2(y);
     bool increment = abs_rem >= abs_half_y + int(y % 2 != 0 || quotient_sign < 0);
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_ties_to_inf(int x, int y) {
@@ -171,7 +180,7 @@ constexpr div_result<int> div_rem_ties_to_neg_inf(int x, int y) {
     int abs_rem = x % y * __sgn2(x);
     int abs_half_y = y / 2 * __sgn2(y);
     bool increment = abs_rem >= abs_half_y + int(y % 2 != 0 || quotient_sign > 0);
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_ties_to_neg_inf(int x, int y) {
@@ -186,7 +195,7 @@ constexpr div_result<int> div_rem_ties_to_odd(int x, int y) {
     int abs_half_y = y / 2 * __sgn2(y);
     int quotient = x / y;
     bool increment = abs_rem >= abs_half_y + int(y % 2 != 0 || quotient % 2 != 0);
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_ties_to_odd(int x, int y) {
@@ -201,7 +210,7 @@ constexpr div_result<int> div_rem_ties_to_even(int x, int y) {
     int abs_half_y = y / 2 * __sgn2(y);
     int quotient = x / y;
     bool increment = abs_rem >= abs_half_y + int(y % 2 != 0 || quotient % 2 == 0);
-    return __div_rem_impl(x, y, increment, quotient_sign);
+    return __div_rem_offset_quotient(x, y, int(increment) * quotient_sign);
 }
 
 constexpr int div_ties_to_even(int x, int y) {
